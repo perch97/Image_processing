2b9971f4a1426ec27c8410ef6a30a7cd172c481b
Vadim Smirnov <110857046+perch97@users.noreply.github.com>
2023-03-12 18:19:23 -0700

    Create main.cpp

---
+++ ImageProcessing_ImageNegative/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "ImageProcessing.h"
+
+int main(){
+    float imgHist[NO_OF_GRAYLEVELS];
+    int imgWidth; int imgHeight; int imgBitDepth;
+    unsigned char imgHeader[BMP_HEADER_SIZE];
+    unsigned char imgColorTable[BMP_COLOR_TABLE_SIZE];
+    unsigned char imgInBuffer[_512by512_IMG_SIZE];
+    unsigned char imgOutBuffer[_512by512_IMG_SIZE];
+    const char imgName[] = "girlface.bmp";
+
+    const char newImgName[] = "girlface_neg.bmp";
+
+    ImageProcessing *myImage = new ImageProcessing(imgName,
+                                 newImgName,
+                                 &imgHeight,
+                                 &imgWidth,
+                                 &imgBitDepth,
+                                 &imgHeader[0],
+                                 &imgColorTable[0],
+                                 &imgInBuffer[0],
+                                 &imgOutBuffer[0]);
+    myImage->readImage();
+    myImage->getImageNegative(imgInBuffer,imgOutBuffer,imgHeight,imgWidth);        
+    myImage->writeImage();
+
+    std::cout<<"Success!"<<std::endl;
+    std::cout<<"Image Height : "<<imgHeight<<std::endl;
+    std::cout<<"Image Width : "<<imgWidth<<std::endl;
+    return 0;
+}
+
+
